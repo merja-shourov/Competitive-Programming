1d864f916556b5ab8f9195a245805bcadd183014
merja-shourov <merjashourov13@gmail.com>
2023-03-25 14:44:22 +0600

    largest value of floting point arrey

--- WEEK#13/soln-02.c
+++ WEEK#13/soln-02.c
@@ -1,11 +1,9 @@
-
-
 /**
  * Question : C Program to find the largest value in a float type array ?
  * 
  * Sample Input : 
- *   Enter array size: 5
- *   Enter arrey's value(floting point): 9.5 4.2 6.4 6.2 7.5
+ *   Enter array size: 4
+ *   Enter arrey's value(floting point): 9.5 4.2 6.4 7.5
  * 
  * Sample Output: 
  *   Largest value of this array: 9.5
@@ -25,8 +23,12 @@ int main(){
           scanf("%f", &num_arr[i]);
      }
 
+     float large = num_arr[0];
      for( i=0; i<n; i++ ){
-
+          if( large < num_arr[i] )
+               large = num_arr[i];
      }
+
+     printf("Largest value of this array: %.2f\n", large);
      return 0;
 }
