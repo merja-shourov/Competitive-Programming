3d922ff3e92b362523ce1ab515cc645cd73ea8bd
merja-shourov <merjashourov13@gmail.com>
2023-03-26 16:18:59 +0600

    smallest value's all index

--- WEEK#13/soln-10.c
+++ WEEK#13/soln-10.c
@@ -1,21 +1,21 @@
 /*
-     Qeustion-08: Write a program that prints ALL indexes in which the smallest value of array is present
+     Qeustion-08: Write a program that prints ALL indexes in which the smallest value of array is present ?
  ?
 
      sample input: 
           Enter array size: 5
-          Enter array's value: 55 77 88 34 645
+          Enter array's value: 44 22 42 42 22;
 
      Sample output:
-          All the indexes of array's large value: 4
+          All the indexes of array's smallest value: 1 4
 
 */
 #include<stdio.h>
 
-int all_index(int arr[], int size, int large_value);
+int all_index(int arr[], int size, int smallest_value);
 
 int main(){
-     int size, i, large_value;
+     int size, i, smallest_value;
      printf("Enter array size: ");
      scanf("%d", &size);
 
@@ -25,30 +25,28 @@ int main(){
           scanf("%d", &arr[i]);
      }
 
-     // find large value
-     large_value = arr[0];
+     // find smallest value
+     smallest_value = arr[0];
      for( i=0; i<size; i++ ){
-          if( large_value < arr[i] ){
-               large_value = arr[i];
+          if( smallest_value > arr[i] ){
+               smallest_value = arr[i];
           }
      }
 
-     all_index(arr, size, large_value);
-
-
+     all_index(arr, size, smallest_value);
 
      return 0;
 }
 
 
-int all_index( int arr[], int size, int large_value ){
+int all_index( int arr[], int size, int smallest_value ){
      
-     printf("All the indexes of large array's value: ");
+     printf("All the indexes of array's smallest value: ");
 
      int i=0;
      while ( i < size ){
 
-          if( arr[i] == large_value ){
+          if( arr[i] == smallest_value ){
                printf("%d ", i);
           }
           i++;
