8554178ccc8a2482fa87815393004f56d9ab42c2
merja-shourov <merjashourov13@gmail.com>
2023-03-28 19:24:31 +0600

    string pass of a function

--- WEEK#14/soln-01.c
+++ WEEK#14/soln-01.c
@@ -1,20 +1,40 @@
 /*
      Question: Passing string as an argument of a function and update that string within the function ?
 
-     Sample input:  
+     Sample input:  Hello World!
 
-     Sapmple Output: 
+     Sapmple Output: hELLO wORLD!
        
 
 */
 
 #include<stdio.h>
+#include<ctype.h>
 
+
+int changeCase(char str[] );
 int main(){
-     
-     char quote[] = "The way to get started is to quit talking and begin doing.";
+     char str[50];
 
+     printf("Enter a string of length < 50: ");
+     fgets(str, 50, stdin);
+
+     // function call
+     changeCase(str);
 
      return 0;
+}
+
+int changeCase(char str[] ){
+     int z;
+     for( z=0; str[z] != '\0'; z++ ){
+          if( isupper(str[z]) )
+               str[z] = tolower(str[z]);
+          else if( islower(str[z]) )
+               str[z] = toupper(str[z]);
+          
+     }
+
+     puts(str);
 }
 
