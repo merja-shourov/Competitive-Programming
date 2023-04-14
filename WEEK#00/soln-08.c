c4254b56ad488498ccfb0962873d6dc5f1cd0d95
Merja Shourov <73456246+merja-shourov@users.noreply.github.com>
2023-04-14 18:44:46 +0600

    swap - using ( + - / * )

--- WEEK#00/soln-08.c
+++ WEEK#00/soln-08.c
@@ -1,15 +1,40 @@
 /*
-Qeustion-08: 
+Qeustion-08: Ask user for two integers a and b. Then swap (interchange) the values of a and b 
+using arithmetic (+, -, *, /) operator. You must not use an extra variable.
+
  
 Sample Input:
+     Enter value of a: 10
+     Enter value of b: 20
 
 Sample Output:
+     After swaping 
+     a = 20 
+     b = 10
 
  */
 
 #include<stdio.h>
 
 int main(){
+     int a,b;
+
+     printf("Enter value of a: ");
+     scanf("%d", &a);
+     printf("Enter value of b: ");
+     scanf("%d", &b);
+
+     a = a+b;  
+     b = a-b; 
+     a = a-b; 
+     // another methon 
+     // a = a*b;
+     // b = a/b;
+     // a = a/b;
+
+
+
+     printf("After swaping \na = %d\nb = %d\n", a, b);
 
      return 0;
 }
