a5f3bcc640eeb904af4feeccd35d7d0756244b23
Merja Shourov <73456246+merja-shourov@users.noreply.github.com>
2023-04-14 18:54:09 +0600

    swap - using bitwise xor operator

--- WEEK#00/soln-09.c
+++ WEEK#00/soln-09.c
@@ -1,15 +1,34 @@
 /*
-Qeustion-09: 
- 
+Qeustion-09: Ask user for two integers a and b. Then swap (interchange) the values of a and b using bitwise XOR (^) operator.
+You must not use an extra variable.
+
 Sample Input:
+     Enter the first number: 5
+     Enter the second number: 9
 
 Sample Output:
+     Before swapping:
+     a = 5, b = 9
+     After swapping:
+     a = 9, b = 5
 
  */
 
 #include<stdio.h>
 
 int main(){
+     int a,b;
+     printf("Enter the first number: ");
+     scanf("%d", &a);
+     printf("Enter the second number: ");
+     scanf("%d", &b);
+
+     printf("Before swapping\na = %d, b = %d\n", a, b);
+
+     a = a^b;
+     b = a^b;
+     a = a^b;
+     printf("After swapping\na = %d, b = %d\n", a, b);
 
      return 0;
 }
