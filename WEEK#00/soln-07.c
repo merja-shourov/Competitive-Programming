db1481657675a0a7bbca0ae22821c717beb8ebc5
Merja Shourov <73456246+merja-shourov@users.noreply.github.com>
2023-04-14 18:13:21 +0600

    swap - using extra variable

--- WEEK#00/soln-07.c
+++ WEEK#00/soln-07.c
@@ -1,15 +1,31 @@
 /*
-Qeustion-07: 
+Qeustion-07: Ask user for two integers a and b. Then swap (interchange) the values of a and b. 
+That means, a should get the value of b and b should get the value of a. Hint: Use an extra variable.
  
 Sample Input:
+     Enter value of a: 10
+     Enter value of b: 20
 
 Sample Output:
+     After swapint a = 20 b = 10
 
  */
 
 #include<stdio.h>
 
 int main(){
+     int a,b,temp;
+
+     printf("Enter value of a: ");
+     scanf("%d", &a);
+     printf("Enter value of b: ");
+
+     scanf("%d", &b);
+     temp = a;
+     a = b;
+     b = temp;
+
+     printf("After swapint a = %d b = %d\n", a, b);
 
      return 0;
 }
