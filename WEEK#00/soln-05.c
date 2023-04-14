c5ff5f87a3a551d83f17a99350e1e8533624b54a
merja-shourov <merjashourov13@gmail.com>
2023-04-14 15:37:41 +0600

    average

--- WEEK#00/soln-05.c
+++ WEEK#00/soln-05.c
@@ -1,15 +1,23 @@
 /*
-Qeustion-05: 
- 
-Sample Input:
+Qeustion-05: Write a C program that reads in 3 numbers and prints their average
 
+Sample Input:
+     enter three integer: 4 2 1
 Sample Output:
+    average: 2.33
 
  */
 
 #include<stdio.h>
 
 int main(){
+     int a,b,c;
+     float avg;
+     printf("enter three integer: ");
+     scanf("%d %d %d", &a, &b, &c);
+     avg = (a+b+c)/3.0;
+
+     printf("average: %.2f\n", avg);
 
      return 0;
 }
