374432fb7dc72d79a7021f81dea144ddc5036183
Merja Shourov <merja_shourov@kali.kali_linux>
2024-08-08 12:06:58 +0600

    python_templete for cp

---
+++ python_templete.py
@@ -0,0 +1,21 @@
+import sys
+# import io, os, 
+# import math, functools, collections
+# import bisect, heapq, numpy
+# alphabets = "abcdefghijklmnopqrstuvwxyz"
+
+
+input   = lambda: sys.stdin.readline().strip()
+inum    = lambda: int(input())
+imap    = lambda: map(int,input().split())
+ilist   = lambda: list(map(int, input().split()))
+
+# sys.stdout.write(str(ans) + "\n")
+
+for _ in range( inum() ):
+    
+
+
+
+
+
