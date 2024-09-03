6c1fd469154190061bc47efa6a8d21fcf23ee932
Merja Shourov <73456246+merja-shourov@users.noreply.github.com>
2024-09-03 14:15:26 +0600

    Update cpp_templete.cpp

--- cpp_templete.cpp
+++ cpp_templete.cpp
@@ -10,6 +10,7 @@ using namespace std;
 void solve(){
     int n; cin >> n;
     vector<int> v(n);
+    
     for( int i=0; i<n; i++ ) cin >> v[i];
     
 }
@@ -17,13 +18,16 @@ void solve(){
 int32_t main(){
     ios_base::sync_with_stdio(0);
     cin.tie(0); cout.tie(0);
-
+     // #ifndef ONLINE_JUDGE
+    //     freopen("input.txt", "r", stdin);
+    //     freopen("output.txt", "w", stdout);
+    // #endif
+    
     int tc = 1;
     // cin >> tc;
     for( int t=1; t <= tc; t++ ){
         // cout << "Case #" << t << ": ";
         solve();
     }
-
     return 0;
 }
